29f90de1ec4c6631c069ecfc8b00669e72b6941a
Rafna Bash <rafnabash@gmail.com>
2018-06-01 21:52:22 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  Bruno: "24K",
+  Elvis: "The Wonder of You",
+  Beyonce: "Irreplaceable"
+};
+
+function updatePlaylist(playlist, 'Nicki', 'Chun-Li'){
+  playlist['Nicki'] = 'Chun-Li'
+  return playlist
+}
